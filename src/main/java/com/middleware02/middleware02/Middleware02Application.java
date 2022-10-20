41eac7c4b6aa35d09d9d6f901b7b700e66441afe
Samuele Catalano <csamuele73@mail.com>
2022-10-20 19:10:30 +0200

    first commit

---
+++ src/main/java/com/middleware02/middleware02/Middleware02Application.java
@@ -0,0 +1,13 @@
+package com.middleware02.middleware02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Middleware02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Middleware02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/middleware02/middleware02/Middleware02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middleware02.middleware02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Middleware02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
